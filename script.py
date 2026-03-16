62d7e91ee1330883765f00b5127df974a547faf0
KWNS-q <dbl42018@outlook.com>
2026-03-16 17:45:41 -0400

    Added user input script

---
+++ script.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+text = input("Enter a word: ")
+
+print("\n------ Result ------")
+print(f"Hello {name}!")
+print(f"Your word in uppercase is: {text.upper()}")
+print("--------------------")
